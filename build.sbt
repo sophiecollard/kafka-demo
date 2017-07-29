3f4ff54ea1bd3ca66af0e542cd67a49fdd871119
Sophie <sophie.collard@hotmail.fr>
2017-07-29 13:41:19 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "kafka-producer-demo"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.11.0.0"
---
+++ src/main/scala/KafkaProducerDemo.scala
@@ -0,0 +1,28 @@
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+
+
+object KafkaProducerDemo {
+
+  def main(args: Array[String]): Unit = {
+    var properties = new Properties()
+
+    properties.setProperty("bootstrap.servers", "127.0.0.1:9092")
+    properties.setProperty("key.serializer", classOf[StringSerializer].getName)
+    properties.setProperty("value.serializer", classOf[StringSerializer].getName)
+
+    properties.setProperty("acks", "1")
+    properties.setProperty("retries", "3")
+    //properties.setProperty("linger.ms", "1") // alternative to producer.flush, sending values every 1 ms
+
+    val producer: Producer[String, String] = new KafkaProducer[String, String](properties)
+
+    val record: ProducerRecord[String, String] = new ProducerRecord[String, String]("first_topic", "3", "message from producer")
+    producer.send(record) // does not actually send the record, only marks it for sending with flush or linger.ms
+    producer.flush() // alternative to linger.ms property, sends all records marked for sending
+
+    producer.close()
+  }
+
+}
